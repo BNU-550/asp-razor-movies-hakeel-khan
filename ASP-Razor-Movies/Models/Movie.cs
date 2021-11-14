5f4d040c4244dc7535467c25260d0f4376c5934e
hakee <hakee@LAPTOP-84U9VAUD>
2021-11-14 22:12:12 +0000

    movie class

---
+++ ASP-Razor-Movies/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_Razor_Movies.Models
+{
+    public class Movies
+    {
+        [Key]
+
+    }
+}
